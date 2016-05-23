cd1868cc394ad9cf4aeb16d9045f0431050a180d
dholiday <davidholiday@hush.com>
2016-05-22 17:56:41 -0700

    lots of new untested code. mostly utility classes

--- src/main/java/com/holitek/smoosh/util/ResourceFileUtil.java
+++ src/main/java/com/holitek/smoosh/util/ResourceFileUtil.java
@@ -1,8 +1,12 @@
 package com.holitek.smoosh.util;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import junit.framework.Assert;
 
 /**
  *  methods to create and read the 31 resource files containing the c(32/k) 
@@ -12,6 +16,76 @@ import java.util.stream.IntStream;
  */
 public class ResourceFileUtil {
 
+	// every answer for c(31|k) where arr[x] = c(31|x+1)
+	// this arrays is 30 elements long because we can assume the answer to
+	// c(31/0)
+	private static final int[] thirtyOneChooseK_ARR = { 31, 
+			                                            495,
+			                                            4995,
+			                                            31465,
+			                                            169911,
+			                                            736281,
+			                                            2629575,
+			                                            7888725,
+			                                            20160075,
+			                                            44352165,
+			                                            84672315,
+			                                            141120525,
+			                                            206253075,
+			                                            265182525,
+			                                            300540195,
+			                                            300540195,
+			                                            265182525,
+			                                            206253075,
+			                                            141120525,
+			                                            84672315,
+			                                            44352165,
+			                                            20160075,
+			                                            7888725,
+			                                            2629575,
+			                                            736281,
+			                                            169911,
+			                                            31465,
+			                                            4995,
+			                                            495,
+			                                            31 };
+	
+	
+	// peak index value for every element table. computed by
+	// arr[n] = *sigma* n=0->n: thirtyOneChooseK_ARR[n]
+	//
+	// note that this array is one element shorter than its counterpart! 
+	private static final int[] thirtyOneChooseK_FloorValueARR = { 30, 
+			                                                      525,
+			                                                      5520,
+			                                                      31990,
+			                                                      175431,
+			                                                      911712,
+			                                                      3541287,
+			                                                      11430012,
+			                                                      31590087,
+			                                                      75942252,
+			                                                      160614567,
+			                                                      301732092,
+			                                                      507988167,
+			                                                      773170692,
+			                                                      1073710887,
+			                                                      1374251082,
+			                                                      1639433607,
+			                                                      1845686682,
+			                                                      1986807207,
+			                                                      2071479522,
+			                                                      2115831687,
+			                                                      2135991762,
+			                                                      2143880487,
+			                                                      2146510062,
+			                                                      2147246343,
+			                                                      2147416254,
+			                                                      2147447719,
+			                                                      2147452714,
+			                                                      2147453209 };	
+
+	
 	/**
 	 * creates the resource files
 	 * @return
@@ -19,8 +93,8 @@ public class ResourceFileUtil {
 	public static boolean buildResourceFiles() {
 		boolean successB = false;
 		
-		Map<Integer, Integer[]> tableElementMap = 
-				new HashMap<Integer, Integer[]>();
+		Map<Integer, List<Integer>> tableElementMap = 
+				new HashMap<Integer, List<Integer>>();
 				
 		int rangeCeilingI = (int)Math.pow(31, 2);		
 				
@@ -47,21 +121,127 @@ public class ResourceFileUtil {
 	
 	
 	
-	
-	public static int elementIndexToTableElementIndex(int elementIndexI) {
-		int returnI = -1;
+	/**
+	 * takes an element index and a table index and combines them into a single
+	 * integer representation. there are thirty-one tables, each containing one
+	 * possible answer to c(n|k). as such, the tables are sized thusly:
+	 * 
+	 * 31/1 --> 31
+     * 31/2 --> 495
+     * 31/3 --> 4995
+     * 31/4 --> 31465
+     * 31/5 --> 169911
+     * 31/6 --> 736281
+     * 31/7 --> 2629575
+     * 31/8 --> 7888725
+     * 31/9 --> 20160075
+     * 31/10 --> 44352165
+     * 31/11 --> 84672315
+     * 31/12 --> 141120525
+     * 31/13 --> 206253075
+     * 31/14 --> 265182525
+     *
+     * 31/15 --> 300540195
+     * 31/16 --> 300540195
+     *
+     * 31/17 --> 265182525
+     * 31/18 --> 206253075
+     * 31/19 --> 141120525
+     * 31/20 --> 84672315
+     * 31/21 --> 44352165
+     * 31/22 --> 20160075
+     * 31/23 --> 7888725
+     * 31/24 --> 2629575
+     * 31/25 --> 736281
+     * 31/26 --> 169911
+     * 31/27 --> 31465
+     * 31/28 --> 4995
+     * 31/29 --> 495
+     * 31/30 --> 31
+     * 
+     * the table a particular index resides in is denoted by the range of the
+     * combined index value. Table 1's index range is 0-29. Table 2's range is
+     * 31-494. Table 3's is 495-4994, etc etc etc. 
+	 * 
+	 * 
+	 * @param elementIndexI
+	 * @param tableIndexI
+	 * @return
+	 */
+	public static int createCombinedElementIndex(
+			int elementIndexI, int tableIndexI) {
 		
+		Assert.assertTrue("tableIndex shouldn't be larger than 29!", 
+				tableIndexI < 30);
+		
+		int returnI = elementIndexI;
+		
+		for (int i = 0; i < tableIndexI; i ++) {
+			returnI += thirtyOneChooseK_ARR[i];
+		}
 		
 		return returnI;
 	}
 	
 	
+	/**
+	 * takes a combined element index and splits it into a 
+	 * tuple -- > (element_index, table_index)
+	 * 
+	 * @param combinedElementIndex
+	 * @return
+	 */
+	public static Pair<Integer, Integer> splitCombinedElementIndex(
+			int combinedElementIndex) {
+				
+		Pair<Integer, Integer> returnPair = null;
+		
+		int elementIndexFloorValue_Index = 
+				thirtyOneChooseK_FloorValueARR.length;
 	
-	public static int TableElementIndexToElementIndex(int tableElementIndexI) {
-		int returnI = -1;
+		int returnElementIndexI = -1;
+		int returnTableIndexI = -1;
 		
+		// check to see if the element is in the last table
+		//
+		int highestTableElementFloorI = 
+				thirtyOneChooseK_FloorValueARR[elementIndexFloorValue_Index];
+						
+		if (combinedElementIndex > highestTableElementFloorI) {
+			returnElementIndexI = 
+					combinedElementIndex - highestTableElementFloorI;
+			
+			returnTableIndexI = thirtyOneChooseK_ARR.length;
+			
+			returnPair = 
+				new Pair<Integer, Integer>
+			        (returnElementIndexI, returnTableIndexI);		
+		}
 		
-		return returnI;
+		// if necessary, run through the remaining options
+		//
+		while ((elementIndexFloorValue_Index > 0) && (returnPair == null)) {
+			elementIndexFloorValue_Index --;
+			
+			int tempIndexI = 
+			  combinedElementIndex -
+			    thirtyOneChooseK_FloorValueARR[elementIndexFloorValue_Index];
+			
+			// if true we've found the floor value
+			if (tempIndexI > 0) {
+				returnElementIndexI = tempIndexI;
+				returnTableIndexI = elementIndexFloorValue_Index + 1;
+				
+				returnPair = 
+						new Pair<Integer, Integer>
+					        (returnElementIndexI, returnTableIndexI);	
+			}
+					
+		}
+		
+		Assert.assertTrue("returnPair shouldn't be null!", returnPair != null);
+		
+		return returnPair;
 	}
 	
 	
@@ -85,7 +265,32 @@ public class ResourceFileUtil {
 		return returnB;
 	}
 	
+
+	
+	/**
+	 * takes an 8-bit binary string, pads it with zeros if necessary, and 
+	 * returns a list containing the indexes where the 1's are located. 
+	 * 
+	 * @param binaryString
+	 * @return
+	 */
+	private static List<Integer> binaryStringToOnesList(String binaryString) {
+		String formattedBinaryString = 
+				String.format("%8s", binaryString.replace(' ', '0'));
+		
+		List<Integer> returnL = 
+				IntStream.range(0, 8)
+		                 .filter(i -> formattedBinaryString.charAt(i) == '1')
+		                 .boxed().collect(Collectors.toList());
+		
+		return returnL;
+	}
+	
+	
 	
 }
+
+
+
 
 
